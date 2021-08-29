a420a2741861e2fa613e6d6e4ab6767c9958b094
Abdelhaq EL AMRAOUI <abdelhaq.el-amraoui@insa.ueuromed.org>
2021-08-29 17:05:16 +0100

    adding offline support

--- Converter.py
+++ Converter.py
@@ -1,27 +1,39 @@
 
 import requests
+import json
 from sys import stderr
 
 def dollor_converter(amount, from_currency, to_currency):
+	from_currency = from_currency.upper()
+	to_currency = to_currency.upper()
+
+	link = "https://open.er-api.com/v6/latest/USD"
+	file_name = from_currency+".json"
+
 	try:
-		link = "https://open.er-api.com/v6/latest/USD"
 		json_data = requests.get(link).json()
 		rates_data = json_data["rates"]
-	except Exception:
-		print("No internet connection !", file=stderr)
-		return None
-	else:
-		if(from_currency.upper() != "USD"):
-			res = amount / rates_data[from_currency.upper()]
-		res = amount * rates_data[to_currency.upper()]
-		return round(res, 4)
-	
 
+		with open(file_name, "w") as jsf:
+			f = str(json_data).replace("\'", "\"")
+			jsf.write(f)
+
+	except Exception:
+		print("----OFFLINE MODE----")
+		with open(file_name, "r") as jsf:
+			data = json.load(jsf)
+			rates_data = data["rates"]	
+
+	if(from_currency != "USD"):
+		res = amount / rates_data[from_currency]
+	else:
+		res = amount * rates_data[to_currency]
+	return round(res, 4)
+	
 amount = int(input("Amount : "))
 from_currency = input("From   : ")
 to_currency = input("To     : ")
 
 res = dollor_converter(amount, from_currency, to_currency)
-
 print("{} {} = {} {}".format(amount, from_currency, res, to_currency))
 
--- test.py
+++ test.py
@@ -1,13 +1,39 @@
+import json
+import ast
+  
+# # Opening JSON file
+# f = open('USD.json',)
+  
+# # returns JSON object as 
+# # a dictionary
+# data = json.load(json.dumps(f))
 
-import requests
 
-def convertor(from_currency,to_currency,money):
-	link="https://open.er-api.com/v6/latest/USD"
-	req=requests.get(link).json()
-	rates_data=req['rates']
-	# if from_currency != 'USD' :
-	# 	from_currency = input("enter usd currency : ")
-	money=round(money *rates_data[to_currency], 4)
-	return money
+# # mixing single and double quotes
+# data = {'jsonKey': 'jsonValue',"title": "hello world"}
 
-print("100$ = ", convertor('USD',"MAD",100))
+# # get string with all double quotes
+# json_string = json.dumps(data) 
+
+# print(json_string)
+#   
+
+with open('USD.json', "r") as jsf:
+	f = jsf.read()
+	f = f.replace('\'', "\"")
+	data =json.dumps(jsf)
+	print(dic)
+	# rates_data = data["rates"]
+
+
+
+# import json
+
+# # mixing single and double quotes
+# data = {'jsonKey': 'jsonValue',"title": "hello world"}
+
+# # get string with all double quotes
+# json_string = json.dumps(data) 
+# print(json_string)
+
+
