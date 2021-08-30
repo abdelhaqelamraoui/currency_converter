be470ed97e11ad1d101d1ab5d6fe2a3b060b8d72
abdelhaqelamraoui <abdelhaqelamraoui@gmail.com>
2021-08-30 18:37:18 +0100

    added : off/on-line status and data outdated test

--- converter.py
+++ converter.py
@@ -3,11 +3,15 @@ import requests
 import json
 import os
 from os import path
-from sys import stderr
 
 
 def write_data_to_file(json_data):
-	dir = "./.json_files/"
+	"""
+	Stores the conversion data locally in a json file : 
+	./files/USD.json. If the parent directory is not exsisting
+	it will be created.
+	"""
+	dir = "./.files/"
 	if not path.exists(dir):
 		os.mkdir(dir)
 	file_name = dir + "USD.json"
@@ -15,29 +19,60 @@ def write_data_to_file(json_data):
 			f = str(json_data).replace("\'", "\"")
 			jsf.write(f)
 
+
+def write_last_update(json_data):
+	"""
+	Writes on a file the time of last update of the conversion data
+	retreived from the website. If the process went wrong False is
+	returned, else it return True.
+	"""
+	with open("./.files/last_update", "wb") as f:
+		try:
+			# json_data = requests.get("https://open.er-api.com/v6/latest/USD").json()
+			tluu = json_data["time_last_update_unix"]
+			encoded_str = str(tluu).encode()#converts a string to a sequence of bytes
+			f.write(encoded_str)
+			return True
+		except Exception:
+			return False		
+	
+
 def all_currencies():
-	link = "https://open.er-api.com/v6/latest/USD"
-	try :
-		json_data = requests.get(link).json()
-		data = json_data["rates"]
-		all_currencies = list([i for i in data.keys()])
-	except Exception:
-		with open("./.json_files/USD.json", "r") as jsf:
-			json_data = json.load(jsf)
-			rates_data = json_data["rates"]
-			all_currencies = list([i for i in rates_data.keys()])
-	else:
-		write_data_to_file(json_data)
-
-	return all_currencies
-
+	"""
+	Returns all the currencies from the website (commented) or
+	from the local list __all_curr_sorted_list
+	"""
+	# link = "https://open.er-api.com/v6/latest/USD"
+	# try :
+	# 	json_data = requests.get(link).json()
+	# 	data = json_data["rates"]
+	# 	all_currencies = list([i for i in data.keys()])
+		# __status = "online"
+	# except Exception:
+	# 	with open("./.files/USD.json", "r") as jsf:
+	# 		json_data = json.load(jsf)
+	# 		rates_data = json_data["rates"]
+	# 		all_currencies = list([i for i in rates_data.keys()])
+	# __status == "offline"
+	# else:
+	# 	if(is_data_outdataed()):
+	# 		write_data_to_file(json_data)
+	# 		write_last_update(json_data)
+	# return all_currencies
+	return __all_curr_sorted_list
 
 
 def convert(amount, from_currency, to_currency):
+	"""
+	Converts an amount in a currency to another.
+	In case of offline mode, conversion is based on data storded
+	locally in a json file from the last online run if data was 
+	outdated.
+	"""
 	from_currency = from_currency.upper()
 	to_currency = to_currency.upper()
 	link = "https://open.er-api.com/v6/latest/USD"
-	file_name = "./.json_files/USD.json"
+	file_name = "./.files/USD.json"
 	
 	try:
 		json_data = requests.get(link).json()
@@ -45,9 +80,11 @@ def convert(amount, from_currency, to_currency):
 	except Exception:
 		with open(file_name, "r") as jsf:
 			data = json.load(jsf)
-			rates_data = data["rates"]	
+			rates_data = data["rates"]
 	else:
-		write_data_to_file(json_data)
+		if(is_data_outdataed(json_data)):
+			write_data_to_file(json_data)
+			write_last_update(json_data)
 
 	if(from_currency == "USD"):
 		res = amount * rates_data[to_currency]
@@ -57,23 +94,52 @@ def convert(amount, from_currency, to_currency):
 		res_1 = convert(amount, from_currency, "USD")
 		res = convert(res_1, "USD", to_currency)
 	return round(res, 4)
-	
-
-# amount = int(input("Amount : "))
-# from_currency = input("From   : ")
-# to_currency = input("To     : ")
-
-# result = convert(amount, from_currency, to_currency)
-# print("{} {} = {} {}".format(amount, from_currency.upper(), result, to_currency.upper()))
 
 
+def get_status():
+	"""
+	Returns "online" if the website providind data is reachable
+	(internet connection is available), else it returns "offline
+	"""
+	try:
+		requests.get("https://open.er-api.com/v6/latest/USD")
+		return "online"
+	except Exception:
+		return "offline"
 
 
-	# else:
-		# for i in all_currencies:
-		# 	link = "https://open.er-api.com/v6/latest/" + i
-		# 	file_name = dir + i +".json"
-		# 	with open(file_name, "w") as jsf:
-		# 		json_data = requests.get(link).json()
-		# 		f = str(json_data).replace("\'", "\"")
-		# 		jsf.write(f)
+def is_data_outdataed(json_data):
+	# json_data = requests.get("https://open.er-api.com/v6/latest/USD").json()
+	time_last_update_unix = json_data["time_last_update_unix"]
+	time_last_update_unix = int(time_last_update_unix)
+	last_update = 0
+	with open("./.files/last_update", "rb") as f:
+		last_update = int(f.read())
+	return (time_last_update_unix > last_update)
+
+
+__all_curr_sorted_list = [
+'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS',
+'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN',
+'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD',
+'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHF',
+'CLP', 'CNY', 'COP', 'CRC', 'CUC', 'CUP', 'CVE',
+'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN',
+'ETB', 'EUR', 'FJD', 'FKP', 'FOK', 'GBP', 'GEL',
+'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD',
+'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS',
+'IMP', 'INR', 'IQD', 'IRR', 'ISK', 'JMD', 'JOD',
+'JPY', 'KES', 'KGS', 'KHR', 'KID', 'KMF', 'KRW',
+'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD',
+'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK',
+'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK', 'MXN',
+'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR',
+'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR',
+'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF',
+'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 
+'SLL', 'SOS', 'SRD', 'SSP', 'STN', 'SYP', 'SZL',
+'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD',
+'TVD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU',
+'UZS', 'VES', 'VND', 'VUV', 'WST', 'XAF', 'XCD',
+'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW']
+
--- gui.py
+++ gui.py
@@ -1,24 +1,25 @@
 
 
 import tkinter as tk
-from converter import *
+import converter as ct
 from tkinter import ttk
 
 
 window = tk.Tk() # main window object
 window.title("Currency Converter")
 
-tk.Label(window, text="Amount").grid(row=0)
-tk.Label(window, text="From").grid(row=1)
-tk.Label(window, text="To").grid(row=2)
-tk.Label(window, text="Result").grid(row=3)
+amount_label = tk.Label(window, text="Amount")
+from_label = tk.Label(window, text="From")
+to_label = tk.Label(window, text="To")
+result_label = tk.Label(window, text="Result")
+status_label = tk.Label(window, text=ct.get_status())
 
 
 amount_w = tk.Entry(window)
 result_w = tk.Entry(window)
 
-currencies = all_currencies()
-currencies.sort()
+currencies = ct.all_currencies()
+
 var_1 = tk.StringVar()
 var_2 = tk.StringVar()
 from_combobox = ttk.Combobox(window, values=currencies, textvariable=var_1)
@@ -29,18 +30,25 @@ def func():
    amt = int(amount_w.get())
    from_curr = from_combobox.get()
    to_curr = to_combobox.get()
-   res = convert(amt, from_curr, to_curr)
+   res = ct.convert(amt, from_curr, to_curr)
    result_w.delete(0, 'end')
    result_w.insert(0, str(res))
 
-button = tk.Button(window, text='Convert', width=10, height=1, command=func)
+
+convert_button = tk.Button(window, text='Convert', width=10, height=1, command=func)
+
+
+amount_label.grid(row=0)
+from_label.grid(row=1)
+to_label.grid(row=2)
+result_label.grid(row=3)
+status_label.grid(row=4, column=0)
 
 amount_w.grid(row=0, column=1)
-
 from_combobox.grid(row=1, column=1)
 to_combobox.grid(row=2, column=1)
 result_w.grid(row=3, column=1)
-button.grid(row=4, column=1)
+convert_button.grid(row=4, column=1)
 
 window.mainloop()
 
